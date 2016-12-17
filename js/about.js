74e34a10747ddc171c57217bedf976f2e28347f9
MThorning <matthewthorning@gmail.com>
2016-12-17 23:17:48 +0000

    fixed about page hashchange issue

--- js/about.js
+++ js/about.js
@@ -1,32 +1,35 @@
 $(function () {
-
     var id;
 
-    updateMenu();
+    buildPage();
 
     //on side menu click
     $(window).on('hashchange', function() {
+        console.log('haschange');
         id = window.location.hash.substring(1);
         updateContent(id);
     });
     
 //FUNCTIONS
 
-    function updateMenu() {
+    function buildPage() {
+        console.log('build page called');
         //side menu creation
         $('#menuItems').html('');
         $.getJSON('about.json', function (about) {
             $.each(about, function (index, element) {
                 $('#menuItems').append('<li><a href="#' + element.id + '">' + element.title + '</a></li>');
             });
-            id = about[0].id;
-            
-            updateContent(id);
+            var oldHash = window.location.hash;
+            window.location.hash = '#' + about[0].id;
+            if (window.location.hash == oldHash) {
+                $(window).trigger('hashchange');
+            }
         });
     }
 
     function updateContent(id) {
-
+        console.log('update to content called');
         //clear content
         $('#content').html('');
         //which page am I looking for?
